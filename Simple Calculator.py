b3116b6faef9b09163423ec954282587b31a12e4
NavinVittayakorn <vin.vittayakorn@gmail.com>
2025-09-02 15:10:46 +0530

    Delete Simple Calculator.py

--- Simple Calculator.py
+++
@@ -1,7 +0,0 @@
-# Simple calculator: addition
-print("Welcome to my addition calculator!: ")
-print("A platform where you can calculate your values: ")
-number1 =float(input("Please enter your First value: "))
-number2 =float(input("Please enter your sceond value: "))
-Result = number1 + number2
-print("The sum is: ", Result)
