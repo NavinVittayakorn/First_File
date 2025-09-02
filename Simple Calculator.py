30ef66f1c7941fd7515430edbbc6b2a8613d824a
NavinVittayakorn <vin.vittayakorn@gmail.com>
2025-09-02 13:44:41 +0530

    Simple Calculator.py
    A simple 1 addition calculator

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -4,4 +4,4 @@ print("A platform where you can calculate your values: ")
 number1 =float(input("Please enter your First value: "))
 number2 =float(input("Please enter your sceond value: "))
 Result = number1 + number2
-print("the sum is: ", Result)
+print("The sum is: ", Result)
